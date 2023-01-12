78b5576e84fac67dfd0882f0920fb039f06932fd
Andrey Fidrya <af@zabiyaka.com>
2023-01-12 04:13:20 +0300

    Make argument parsing code more readable

--- Sources/rmud/Controllers/Creature+CommandInterpreter.swift
+++ Sources/rmud/Controllers/Creature+CommandInterpreter.swift
@@ -1,101 +1,25 @@
 import Foundation
 
 extension Creature {
-    func interpretCommand(_ command: String) {
-        // Cancel direction commands if any non-direction command is typed or even ENTER is pressed
-        var wasDirectionCommand = false
-        defer {
-            if !wasDirectionCommand && !movementPath.isEmpty {
-                movementPath.removeAll()
+    private struct FetchArgumentContext {
+        var targetName: String? = nil
+        var targetStartIndex = 1
+        var targetAmount = 1
+        
+        var currentIndex = 1
+        var objectsAdded = 0
+        
+        init(word: String, isCountable: Bool = true, isMany: Bool = false) {
+            if isCountable {
+                (targetName, targetStartIndex, targetAmount) = Self.extractIndexAndAmount(word)
             }
-        }
-        
-        let trimmed = command.trimmingCharacters(in: .whitespaces)
-        
-        // Just drop to next line for hitting CR
-        guard !trimmed.isEmpty else { return }
-
-        let scanner = Scanner(string: trimmed)
-        
-        guard let commandPrefix = scanner.scanUpToCharacters(from: .whitespaces) else {
-            return
-        }
-        
-        let nonOptionalArgumentMissing: (_ what: CommandArgumentFlags.What)->() = { what in
-            // TODO: multiple items/creatures?
-            if what.contains(anyOf: [.word, .restOfString]) {
-                self.send("Пожалуйста, укажите аргумент.")
-            } else if what.contains(.creature) && what.contains(.item) {
-                self.send("Пожалуйста, укажите персонажа или предмет.")
-            } else if what.contains(.creature) {
-                self.send("Пожалуйста, укажите персонажа.")
-            } else if what.contains(.item) {
-                self.send("Пожалуйста, укажите предмет.")
-            } else {
-                self.send("Пожалуйста, укажите аргумент.")
-            }
-        }
-
-        
-        
-        var found = false
-        let fullDirections = preferenceFlags?.contains(.fullDirections) ?? false
-        commandInterpreter.enumerateCommands(roles: player?.roles ?? [], commandPrefix: commandPrefix, fullDirections: fullDirections) { command, stop in
             
-            found = true
-            stop = true
-
-            wasDirectionCommand = command.group == .movement && command.flags.contains(.directionCommand)
-
-            var context = CommandContext(command: command, scanner: scanner)
-            context.subcommand = command.subcommand
-            guard fetchArgument(from: scanner,
-                                what: command.arg1What,
-                                where: command.arg1Where,
-                                cases: command.arg1Cases,
-                                intoCreatures: &context.creatures1,
-                                intoItems: &context.items1,
-                                intoArgument: &context.argument1) else {
-                nonOptionalArgumentMissing(command.arg1What)
-                return
-                                    
-            }
-            guard fetchArgument(from: scanner,
-                                what: command.arg2What,
-                                where: command.arg2Where,
-                                cases: command.arg2Cases,
-                                intoCreatures: &context.creatures2,
-                                intoItems: &context.items2,
-                                intoArgument: &context.argument2) else {
-                nonOptionalArgumentMissing(command.arg2What)
-                return
-            }
-            if let handler = command.handler {
-                handler(self)(context)
-            } else {
-                send("Эта команда всё ещё не реализована.")
+            if !isMany {
+                targetAmount = 1
             }
         }
-        
-        if !found {
-            send("Хмм?")
-        }
-    }
     
-    private func fetchArgument(from scanner: Scanner,
-                               what: CommandArgumentFlags.What,
-                               where whereAt: CommandArgumentFlags.Where,
-                               cases: GrammaticalCases,
-                               intoCreatures: inout [Creature],
-                               intoItems: inout [Item],
-                               intoArgument: inout String) -> Bool {
-        
-        // Exit early if no argument was requested
-        guard what.contains(anyOf: [.creature, .item, .word, .restOfString]) else {
-            return true
-        }
-
-        let extractIndexAndAmount: (_ word: String) -> (name: String?, startIndex: Int, amount: Int) = { word in
+        static func extractIndexAndAmount(_ word: String) -> (name: String?, startIndex: Int, amount: Int) {
             if word.isEqual(toOneOf: ["все", "всем", "all"], caseInsensitive: true) {
                 return (nil, 1, Int.max)
             }
@@ -124,7 +48,120 @@ extension Creature {
             }
             return (scanner.textToParse.trimmingCharacters(in: .whitespaces), max(startIndex, 1), max(amount, 1))
         }
+    }
+    
+    func interpretCommand(_ command: String) {
+        // Cancel direction commands if any non-direction command is typed or even ENTER is pressed
+        var wasDirectionCommand = false
+        defer {
+            if !wasDirectionCommand && !movementPath.isEmpty {
+                movementPath.removeAll()
+            }
+        }
         
+        let trimmed = command.trimmingCharacters(in: .whitespaces)
+        
+        // Just drop to next line for hitting CR
+        guard !trimmed.isEmpty else { return }
+
+        let scanner = Scanner(string: trimmed)
+        
+        guard let commandPrefix = scanner.scanUpToCharacters(from: .whitespaces) else {
+            return
+        }
+        
+        var found = false
+        let fullDirections = preferenceFlags?.contains(.fullDirections) ?? false
+        commandInterpreter.enumerateCommands(roles: player?.roles ?? [], commandPrefix: commandPrefix, fullDirections: fullDirections) { command, stop in
+            
+            found = true
+            stop = true
+
+            wasDirectionCommand = command.group == .movement && command.flags.contains(.directionCommand)
+
+            var context = CommandContext(command: command, scanner: scanner)
+            context.subcommand = command.subcommand
+            fetchArgument(from: scanner,
+                          what: command.arg1What,
+                          where: command.arg1Where,
+                          cases: command.arg1Cases,
+                          intoCreatures: &context.creatures1,
+                          intoItems: &context.items1,
+                          intoString: &context.argument1)
+            fetchArgument(from: scanner,
+                          what: command.arg2What,
+                          where: command.arg2Where,
+                          cases: command.arg2Cases,
+                          intoCreatures: &context.creatures2,
+                          intoItems: &context.items2,
+                          intoString: &context.argument2)
+            if let handler = command.handler {
+                handler(self)(context)
+            } else {
+                send("Эта команда всё ещё не реализована.")
+            }
+        }
+        
+        if !found {
+            send("Хмм?")
+        }
+    }
+    
+    private func fetchSelf(word: String, into: inout [Creature]) -> Bool {
+        if word.isEqual(toOneOf: ["себя", "себе", "собой", "я", "меня", "мне", "мной", "i", "self", "me"], caseInsensitive: true) {
+            into.append(self)
+            return true
+        }
+        return false
+    }
+    
+    private func fetchItemsInInventory(context: inout FetchArgumentContext, into: inout [Item], cases: GrammaticalCases) -> Bool {
+        for item in carrying {
+            guard context.targetName == nil || item.isAbbrevOfNameOrSynonym(context.targetName!, cases: cases) else { continue }
+            guard /* extra.contains(.notOnlyVisible) || */ canSee(item) else { continue }
+            defer { context.currentIndex += 1 }
+            guard context.currentIndex >= context.targetStartIndex else { continue }
+            
+            context.objectsAdded += 1
+            into.append(item)
+            
+            guard context.objectsAdded < context.targetAmount else { return true }
+        }
+        return false
+    }
+    
+    private func fetchCreaturesInRoom(context: inout FetchArgumentContext, into: inout [Creature], cases: GrammaticalCases, isNoMobile: Bool = false) -> Bool {
+        guard let roomCreatures = inRoom?.creatures else { return false }
+        
+        for creature in roomCreatures {
+            guard creature.isPlayer || !isNoMobile else { continue }
+            guard context.targetName == nil || creature.isAbbrevOfNameOrSynonym(context.targetName!, cases: cases) else { continue }
+            guard /* extra.contains(.notOnlyVisible) || */ canSee(creature) else { continue }
+            
+            defer { context.currentIndex += 1 }
+            guard context.currentIndex >= context.targetStartIndex else { continue }
+            
+            context.objectsAdded += 1
+            into.append(creature)
+            
+            guard context.objectsAdded < context.targetAmount else { return true }
+        }
+        
+        return false
+    }
+    
+    func fetchArgument(from scanner: Scanner,
+                               what: CommandArgumentFlags.What,
+                               where whereAt: CommandArgumentFlags.Where,
+                               cases: GrammaticalCases,
+                               intoCreatures: inout [Creature],
+                               intoItems: inout [Item],
+                               intoString: inout String) {
+        // Exit early if no argument was requested
+        guard what.contains(anyOf: [.creature, .item, .word, .restOfString]) else {
+            return
+        }
+
         // Try to process argument in following order:
         // - as me
         // - items worn / equipped
@@ -139,67 +176,36 @@ extension Creature {
         // Creatures, items and words are described by single word, so try to read one from input:
         let originalScanLocation = scanner.scanLocation // in case we need to undo word read later
         guard let word = scanner.scanUpToCharacters(from: .whitespaces) else {
-            return true // all args are optional now // extra.contains(.optional)
+            return
         }
         
         // - as me
         // FIXME: move all reserved keywords to a class, assign variables to them and don't reference them as text,
         // exclude them from valid names
-        if what.contains(.creature) && whereAt.contains(anyOf: [.room, .world]) &&
-                word.isEqual(toOneOf: ["себя", "себе", "собой", "я", "меня", "мне", "мной",
-                                       "i", "self", "me"], caseInsensitive: true) {
-            intoCreatures.append(self)
-            return true
+        if what.contains(.creature) && whereAt.contains(anyOf: [.room, .world]) {
+            if fetchSelf(word: word, into: &intoCreatures) {
+                return
+            }
         }
         
-        var targetName: String? = nil
-        var targetStartIndex = 1
-        var targetAmount = 1
-        if what.contains(anyOf: [.creature, .item]) {
-            (targetName, targetStartIndex, targetAmount) = extractIndexAndAmount(word)
-        }
-        //print("fetchArgument: name=\(targetName ?? "nil"), index=\(targetIndex), amount=\(amount)")
-
-        if !what.contains(.many) {
-            targetAmount = 1
-        }
-        
-        var currentIndex = 1
-        var objectsAdded = 0
+        let isCountable = what.contains(anyOf: [.creature, .item])
+        let isMany = what.contains(.many)
+        var context = FetchArgumentContext(word: word, isCountable: isCountable, isMany: isMany)
 
         // - items worn / equipped
         // TODO
         
         // - items in inventory
         if what.contains(.item) && whereAt.contains(anyOf: [.inventory, .world]) {
-            for item in carrying {
-                guard targetName == nil || item.isAbbrevOfNameOrSynonym(targetName!, cases: cases) else { continue }
-                guard /* extra.contains(.notOnlyVisible) || */ canSee(item) else { continue }
-                defer { currentIndex += 1 }
-                guard currentIndex >= targetStartIndex else { continue }
-                
-                objectsAdded += 1
-                intoItems.append(item)
-
-                guard objectsAdded < targetAmount else { return true }
+            if fetchItemsInInventory(context: &context, into: &intoItems, cases: cases) {
+                return
             }
         }
 
         // - creatures in room
-        if what.contains(.creature), whereAt.contains(anyOf: [.room, .world]), let roomCreatures = inRoom?.creatures {
-            for creature in roomCreatures {
-                
-                guard creature.isPlayer || !what.contains(.noMobile) else { continue }
-                guard targetName == nil || creature.isAbbrevOfNameOrSynonym(targetName!, cases: cases) else { continue }
-                guard /* extra.contains(.notOnlyVisible) || */ canSee(creature) else { continue }
-
-                defer { currentIndex += 1 }
-                guard currentIndex >= targetStartIndex else { continue }
-                
-                objectsAdded += 1
-                intoCreatures.append(creature)
-                
-                guard objectsAdded < targetAmount else { return true }
+        if what.contains(.creature), whereAt.contains(anyOf: [.room, .world]) {
+            if fetchCreaturesInRoom(context: &context, into: &intoCreatures, cases: cases, isNoMobile: what.contains(.noMobile)) {
+                return
             }
         }
 
@@ -214,17 +220,15 @@ extension Creature {
 
         // - as word
         if what.contains(.word) {
-            intoArgument = word
-            return true
+            intoString = word
+            return
         }
 
         // - as rest of string
         if what.contains(.restOfString) {
             scanner.scanLocation = originalScanLocation // undo word read
-            intoArgument = scanner.textToParse.trimmingCharacters(in: .whitespaces)
-            return true
+            intoString = scanner.textToParse.trimmingCharacters(in: .whitespaces)
+            return
         }
-
-        return true
     }
 }
