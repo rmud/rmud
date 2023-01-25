dae75b4d1241032bc1e987322ce76085f2758fb2
Andrey Fidrya <af@zabiyaka.com>
2023-01-25 22:52:05 +0300

    Swap gender enum indexes to match act() string order

--- Sources/rmud/Data/Gender.swift
+++ Sources/rmud/Data/Gender.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 enum Gender: UInt8 {
-    case neuter = 0
-    case masculine = 1
-    case feminine = 2
+    case masculine = 0
+    case feminine = 1
+    case neuter = 2
     case plural = 3
     case genderNotSpecifiedForCorpse = 0xff // FIXME: shouldn't be here
     
     var singleLetter: String {
         switch self {
-        case .neuter: return "н"
         case .masculine: return "м"
         case .feminine: return "ж"
+        case .neuter: return "н"
         case .plural: return "*"
         case .genderNotSpecifiedForCorpse: return "т"
         }
@@ -26,9 +26,9 @@ enum Gender: UInt8 {
     
     static func registerDefinitions(in e: Enumerations) {
         e.add(aliases: aliases, namesByValue: [
-            0: "средний",
-            1: "мужской",
-            2: "женский",
+            0: "мужской",
+            1: "женский",
+            2: "средний",
             3: "множественный"
         ])
     }
