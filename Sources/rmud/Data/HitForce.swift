325e8f09275d28099a59b0c1bdee887d8adb620a
Andrey Fidrya <af@zabiyaka.com>
2023-01-26 05:41:13 +0300

    Remove tickle damage level

--- Sources/rmud/Data/HitForce.swift
+++ Sources/rmud/Data/HitForce.swift
@@ -2,7 +2,6 @@ import Foundation
 
 enum HitForce {
     case noDamage
-    case tickle
     case bruise
     case barely
     case wound
@@ -16,8 +15,6 @@ enum HitForce {
     init(damage: Int) {
         if damage <= 0 {
             self = .noDamage
-        } else if damage <= 1 {
-            self = .tickle
         } else if damage <= 2 {
             self = .bruise
         } else if damage <= 4 {
@@ -42,7 +39,6 @@ enum HitForce {
     var attacker: String {
         switch self {
         case .noDamage: return "Вы попытались &1 2в, но не нанесли вреда."
-        case .tickle: return "Вы пощекотали 2в, &2 2(его,ее,его,их)."
         case .bruise: return "Вы оцарапали 2в, &2 2(его,ее,его,их)."
         case .barely: return "Вы слегка задели 2в, &2 2(его,ее,его,их)."
         case .wound: return "Вы легко ранили 2в, &2 2(его,ее,его,их)."
@@ -58,7 +54,6 @@ enum HitForce {
     var victim: String {
         switch self {
         case .noDamage: return "1и попытал1(ся,ась,ось,ись) &1 ВАС, но не нанес1(ла,ло,ли) вреда."
-        case .tickle: return "1и пощекотал1(,а,о,и) ВАС, &2."
         case .bruise: return "1и оцарапал1(,а,о,и) ВАС, &2."
         case .barely: return "1и слегка задел1(,а,о,и) ВАС, &2."
         case .wound: return "1и легко ранил1(,а,о,и) ВАС, &2."
@@ -74,7 +69,6 @@ enum HitForce {
     var room: String {
         switch self {
         case .noDamage: return "1и попытал1(ся,ась,ось,ись) &1 2в, но не нанес1(,ла,ло,ли) вреда."
-        case .tickle: return "1и пощекотал1(,а,о,и) 2в, &2 2(его,ее,его,их)."
         case .bruise: return "1и оцарапал1(,а,о,и) 2в, &2 2(его,ее,его,их)."
         case .barely: return "1и слегка задел1(,а,о,и) 2в, &2 2(его,ее,его,их)."
         case .wound: return "1и легко ранил1(,а,о,и) 2в, &2 2(его,ее,его,их)."
