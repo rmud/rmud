8ba6fbbb564343eb274179032228263e7af17425
Andrey Fidrya <af@zabiyaka.com>
2023-02-20 02:13:42 +0300

    Stack put command output

--- Sources/rmud/Commands/Item/doPut.swift
+++ Sources/rmud/Commands/Item/doPut.swift
@@ -16,21 +16,31 @@ extension Creature {
             return
         }
         
+        let stacker = StringStacker()
+
         for item in items {
             if item !== whereTo {
                 let containedWeight =
                     whereTo.weightWithContents() - whereTo.weight + item.weight
                 guard containedWeight <= container.capacity else {
-                    act("@1и в @2п не помест@1(и,и,и,я)тся.", .to(self), .item(item), .item(whereTo))
+                    act("@1и в @2п не помест@1(и,и,и,я)тся.", .to(self), .item(item), .item(whereTo)) { target, output in
+                        stacker.collect(target: target, line: output)
+                    }
                     continue
                 }
-                act("Вы положили @1в в @2в.", .to(self), .item(item), .item(whereTo))
-                act("1*и положил1(,а,о,и) @1в в @2в.", .toRoom, .excluding(self), .item(item), .item(whereTo))
+                act("Вы положили @1в в @2в.", .to(self), .item(item), .item(whereTo)) { target, output in
+                    stacker.collect(target: target, line: output)
+                }
+                act("1*и положил1(,а,о,и) @1в в @2в.", .toRoom, .excluding(self), .item(item), .item(whereTo)) { target, output in
+                    stacker.collect(target: target, line: output)
+                }
                 item.removeFromCreature()
                 item.put(into: whereTo)
             } else {
-                send("Предметы нельзя класть внутрь себя.")
+                stacker.collect(target: self, line: "Предметы нельзя класть внутрь себя.")
             }
         }
+        
+        stacker.send()
     }
 }
