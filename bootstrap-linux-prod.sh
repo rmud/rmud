f71668dd87fb2be0f750e759e34a4d018aaf952e
Andrey Fidrya <af@zabiyaka.com>
2023-11-10 18:40:29 +0300

    Update Linux bootstrap script

--- bootstrap-linux-prod.sh
+++ bootstrap-linux-prod.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-sudo apt install php7.2 php7.2-common php7.2-cli php7.2-fpm php7.2-mbstring
+
+# Ubuntu 18
+# sudo apt install php7.2 php7.2-common php7.2-cli php7.2-fpm php7.2-mbstring
+
+# Ubuntu 22
+sudo apt install php-cli php-mbstring
 
